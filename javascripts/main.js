3ab01adafb29943e741f9f6f0b58415ffb8c31ee
slamcode <pascal@slamcode.com>
2015-07-14 13:35:39 +0200

    Added new demo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,14 @@
 var cato = new Cato({
-          'tooltips': true,
-          'width': 900,
-          'height': 600
-        })
+            'tooltips': true,
+            'width': 900,
+            'height': 600
+          })
 cato.createSlider('custom')
+
+var catoVertical = new Cato({
+                    'tooltips': true,
+                    'width': 900,
+                    'height': 600,
+                    'direction': 'vertical'
+                  })
+catoVertical.createSlider('custom-vertical')
