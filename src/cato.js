105f80da009c9a8efc1319c81c1c1f83a6d3a01b
Pascal <pascal@slamcode.com>
2018-11-07 10:28:46 +0100

    Major refactoring (#3)
    * started refactoring
    
    * fixed options for filters, removed unnecessary webkit check/prefixes
    
    * removed clutter
    
    * ignore this :)
    
    * removed
    
    * switching to modern javascript
    
    * switched to webpack for building
    
    * demo now shows vertical and horizontal sliders
    
    * added jest & babel for testing
    
    * added eslint & prettier
    
    * updated readme

--- src/cato.js
+++ src/cato.js
@@ -1,31 +1,30 @@
+'use strict'
 // Compare Apples To Oranges
 
-function Cato (options) {
-  'use strict'
-  var opts = options || {}
-
-  this.options = {
-    'tooltips': opts.tooltips || false,
-    'direction': opts.direction || 'horizontal',
-    'width': opts.width || 700,
-    'height': opts.height || 450,
-    'initial': opts.initial || 30,
-    'filter': {
-      'active': opts.filter_active || false,
-      'effect': opts.filter_effect || false
+export default class Cato {
+  constructor(options) {
+    this.options = {
+      tooltips: options.tooltips || false,
+      direction: options.direction || 'horizontal',
+      width: options.width || 700,
+      height: options.height || 450,
+      initial: options.initial || 30,
+      filter: {
+        active: options.filter.active || false,
+        effect: options.filter.effect || null,
+      },
     }
   }
 
-  Cato.prototype.createSlider = function (id) {
-
-    var container = document.getElementById(id),
-        containerInner = container.children,
-        imgToSlide = containerInner[0],
-        imgBase = containerInner[1],
-        range = containerInner[2],
-        output = containerInner[3],
-        outputTriangle = containerInner[4],
-        self = this
+  createSlider(el) {
+    const container = el
+    const containerInner = container.children
+    const imgToSlide = containerInner[0]
+    const imgBase = containerInner[1]
+    const range = containerInner[2]
+    const output = containerInner[3]
+    const outputTriangle = containerInner[4]
+    const self = this
 
     // INITIAL STYLINGS
     addClass(container, 'comparison')
@@ -41,126 +40,132 @@ function Cato (options) {
     range.style.top = imgBase.getBoundingClientRect().height + 'px'
     range.style.width = imgBase.width + 'px'
     output.style.top = imgBase.getBoundingClientRect().height - 40 + 'px'
-    outputTriangle.style.top = imgBase.getBoundingClientRect().height - 15 + 'px'
+    outputTriangle.style.top =
+      imgBase.getBoundingClientRect().height - 15 + 'px'
     range.value = self.options.initial
 
     // initial overlap
-    if (isWebkit()) {
-      var initialClip = (self.options.direction === 'horizontal') ? imgBase.width * self.options.initial / 100 : imgBase.height * self.options.initial / 100
-      imgBase.style.webkitClipPath = setInsetDirection(self.options.direction, initialClip)
-    } else {
-      imgBase.style.clip = 'rect(0px, 30px, 450px, 0px)'
-    }
+
+    const initialClip =
+      self.options.direction === 'horizontal'
+        ? (imgBase.width * self.options.initial) / 100
+        : (imgBase.height * self.options.initial) / 100
+    imgBase.style.clipPath = setInsetDirection(
+      self.options.direction,
+      initialClip,
+    )
 
     // flip input range and adjust to the side if vertical
     if (self.options.direction === 'vertical') {
       range.style.transform = 'rotate(90deg)'
       range.style.width = imgBase.getBoundingClientRect().height + 'px'
-      range.style.left = imgBase.getBoundingClientRect().right - range.getBoundingClientRect().left + 7 + 'px'
+      range.style.left =
+        imgBase.getBoundingClientRect().right -
+        range.getBoundingClientRect().left +
+        7 +
+        'px'
       range.style.top = imgBase.getBoundingClientRect().height / 2 + 'px'
       range.style.margin = '-1px 3px 1px'
     }
 
     // Applying Filters if any
     if (self.options.filter.active) {
-      if (isWebkit()) {
-        imgToSlide.style.webkitFilter = self.options.filter.effect
-      } else {
-        imgToSlide.style.filter = self.options.filter.effect
-      }
+      imgToSlide.style.filter = self.options.filter.effect
     }
 
     // EVENT REGISTRATIONS
 
-    range.addEventListener('input', function () {
+    range.addEventListener('input', function() {
       handleSlides(self)
     })
 
     if (self.options.tooltips) {
-      range.addEventListener('focus', function () {
+      range.addEventListener('focus', function() {
         handleFocus()
       })
 
-      range.addEventListener('blur', function () {
+      range.addEventListener('blur', function() {
         handleBlur()
       })
     }
 
     // EVENT HANDLERS
 
-    var handleSlides = function (self) {
-      var width = imgBase.width,
-          height = imgBase.getBoundingClientRect().height,
-          slidedWith = (self.options.direction === 'horizontal') ? width * range.value / 100 : height * range.value / 100
+    const handleSlides = function(self) {
+      const width = imgBase.width
+      const height = imgBase.getBoundingClientRect().height
+      const slidedWith =
+        self.options.direction === 'horizontal'
+          ? (width * range.value) / 100
+          : (height * range.value) / 100
 
-      if (isWebkit()) {
-        imgBase.style.webkitClipPath = setInsetDirection(self.options.direction, slidedWith)
-      } else {
-        imgBase.style.clip = 'rect(0px, ' + slidedWith + 'px, 450px, 0px)'
-      }
-
+      imgBase.style.clipPath = setInsetDirection(
+        self.options.direction,
+        slidedWith,
+      )
       output.style.left = slidedWith + 'px'
       outputTriangle.style.left = slidedWith + 'px'
 
       if (self.options.direction === 'vertical') {
-        output.style.left = imgBase.getBoundingClientRect().right - range.getBoundingClientRect().left - 45 + 'px'
-        outputTriangle.style.left = imgBase.getBoundingClientRect().right - range.getBoundingClientRect().left - 22 + 'px'
+        output.style.left =
+          imgBase.getBoundingClientRect().right -
+          range.getBoundingClientRect().left -
+          45 +
+          'px'
+        outputTriangle.style.left =
+          imgBase.getBoundingClientRect().right -
+          range.getBoundingClientRect().left -
+          22 +
+          'px'
         output.style.top = slidedWith - 10 + 'px'
         outputTriangle.style.top = slidedWith + 'px'
         outputTriangle.style.transform = 'rotate(-90deg)'
       }
 
       output.setAttribute('data-range', range.value.toString(10))
-
     }
 
-    var handleFocus = function () {
+    const handleFocus = function() {
       addClass(output, 'active')
       addClass(outputTriangle, 'active')
     }
 
-    var handleBlur = function () {
+    const handleBlur = function() {
       removeClass(output, 'active')
       removeClass(outputTriangle, 'active')
     }
-
   }
+}
 
-  // HELPERS
+// HELPERS
 
-  var addClass = function (el, className) {
-    // see youmightnotneedjquery.com
-    if (el.classList) {
-      el.classList.add(className)
-    } else {
-      el.className += ' ' + className
-    }
+const addClass = function(el, className) {
+  // see youmightnotneedjquery.com
+  if (el.classList) {
+    el.classList.add(className)
+  } else {
+    el.className += ' ' + className
   }
+}
 
-  var removeClass = function (el, className) {
-    // see youmightnotneedjquery.com
-    if (el.classList) {
-      el.classList.remove(className)
-    } else {
-      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
-    }
+const removeClass = function(el, className) {
+  // see youmightnotneedjquery.com
+  if (el.classList) {
+    el.classList.remove(className)
+  } else {
+    el.className = el.className.replace(
+      new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'),
+      ' ',
+    )
   }
+}
 
-  var isWebkit = function () {
-    var ua = window.navigator.userAgent.toLowerCase()
-    if ((/webkit/).test(ua)) {
-      return true
-    }
+const setInsetDirection = function(direction, val) {
+  if (direction === 'horizontal') {
+    return 'inset(0px 0px 0px ' + val + 'px)'
+  } else if (direction === 'vertical') {
+    return 'inset(' + val + 'px 0px 0px 0px)'
+  } else {
+    throw new Error('Direction must be either horizontal or vertical')
   }
-
-  var setInsetDirection = function (direction, val) {
-    if (direction === 'horizontal') {
-      return 'inset(0px 0px 0px ' + val + 'px)'
-    } else if (direction === 'vertical') {
-      return 'inset(' + val + 'px 0px 0px 0px)'
-    } else {
-      throw new Error('Direction must be either horizontal or vertical')
-    }
-  }
-
 }
---
+++ tests/cato.test.js
@@ -0,0 +1,28 @@
+import Cato from '../src/cato.js'
+
+describe('Cato', () => {
+  describe('smoke:', () => {
+    it('should take an options argument', () => {
+      const options = {
+        'tooltips': true,
+        'direction': 'horizontal',
+        'width': 700,
+        'height': 450,
+        'initial': 30,
+         'filter': {
+            'active': true,
+            'effect': 'sepia(50%)'
+          }
+        }
+      
+      const slider = new Cato(options)
+      expect(slider.options.tooltips).toBe(true)
+      expect(slider.options.direction).toBe('horizontal')
+      expect(slider.options.width).toBe(700)
+      expect(slider.options.height).toBe(450)
+      expect(slider.options.initial).toBe(30)
+      expect(slider.options.filter.active).toBe(true)
+      expect(slider.options.filter.effect).toBe('sepia(50%)')
+    })
+  })
+})
