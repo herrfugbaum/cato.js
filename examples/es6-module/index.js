821cb9f5fc0d735e987d55587bd29d863eed6d6a
Pascal Spiesz <pascal@slamcode.com>
2018-11-07 19:33:14 +0100

    cleaned up demo; renamed demo to examples; added es6 module example

---
+++ examples/es6-module/index.js
@@ -0,0 +1,37 @@
+import Cato from './../../src/cato.js'
+
+const optionsFirstSlider = {
+  // are optional
+  tooltips: true, // boolean
+  direction: 'vertical', // string horizontal & vertical
+  width: 700, // integer default = 700px
+  height: 450, // integer default = 450px
+  initial: 30, // integer default = 30px (initial position for slider in px)
+  filter: {
+    active: true, // boolean
+    effect:
+      'grayscale(200%)' /* url, blur, brightness, contrast, drop-shadow, grayscale, hue-rotate, invert, opacity, saturate, sepia */,
+  },
+}
+
+const optionsSecondSlider = {
+  // are optional
+  tooltips: true, // boolean
+  direction: 'horizontal', // string horizontal & vertical
+  width: 700, // integer default = 700px
+  height: 450, // integer default = 450px
+  initial: 30, // integer default = 30px (initial position for slider in px)
+  filter: {
+    active: true, // boolean
+    effect:
+      'sepia(50%)' /* url, blur, brightness, contrast, drop-shadow, grayscale, hue-rotate, invert, opacity, saturate, sepia */,
+  },
+}
+
+const firstSlider = new Cato(optionsFirstSlider)
+
+firstSlider.createSlider(document.getElementById('first-slider'))
+
+const secondSlider = new Cato(optionsSecondSlider)
+
+secondSlider.createSlider(document.getElementById('second-slider'))
