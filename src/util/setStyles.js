48b3ef2cb98d35a8d798ed2bae6fb0779967ad55
Rasti Tkac <info@rastitkac.net>
2020-02-23 13:39:15 +0100

    Add vendor prefixing (#20)
    * Import and use setVendor
    
    * Define setVendor
    
    * setVendor is used to vendor prefix js style properties

--- src/util/setStyles.js
+++ src/util/setStyles.js
@@ -2,4 +2,12 @@ const setStyles = (el, styles) => {
   Object.assign(el.style, styles)
 }
 
-export { setStyles }
+const setVendor = (el, property, value) => {
+	el.style['Webkit' + property] = value;
+	el.style['Moz' + property] = value;
+	el.style['ms' + property] = value;
+	el.style['o' + property] = value;
+	el.style.property = value;
+}
+
+export { setStyles, setVendor }
